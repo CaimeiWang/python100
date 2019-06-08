d25edcc7b32895a897614fef9a42b571d814813f
CaimeiWang <1121368912@qq.com>
2019-06-08 13:46:14 +0800

    add 084

---
+++ 084.py
@@ -0,0 +1,13 @@
+'''
+连接字符串
+'''
+# ##method1:
+# a='hello'
+# b='python'
+# c=a+' '+b
+# print(c)
+
+##method2:
+delimiter = ','
+mylist = ['Brazil', 'Russia', 'India', 'China']
+print(delimiter.join(mylist))
